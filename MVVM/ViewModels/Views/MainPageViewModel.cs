9f294ab21c8ab658400574f5b0098be462cf5ba7
Михаил Александренко <aleksandrenko.2@mail.ru>
2022-02-22 20:14:22 +0400

    Удаленное подключение к БД

--- MVVM/ViewModels/Views/MainPageViewModel.cs
+++ MVVM/ViewModels/Views/MainPageViewModel.cs
@@ -18,9 +18,9 @@ namespace MVVM.ViewModels.Views
             {
                 SqlDataAdapter NewsAdapter = new SqlDataAdapter();
                 SqlDataAdapter ImportantInformationAdapter = new SqlDataAdapter();
-                NewsAdapter.SelectCommand = new SqlCommand("select Top(6) [id_news],[date_publication],[heading],[news] from [News] ORDER BY [id_news] desc", connection);
+                NewsAdapter.SelectCommand = new SqlCommand("select Top(5) [id_news],[date_publication],[heading],[news] from [News] ORDER BY [id_news] desc", connection);
                 NewsAdapter.Fill(DataTableBaseNewsPresenter);
-                ImportantInformationAdapter.SelectCommand = new SqlCommand("select Top(6) [id_important_information],[date_publication],[heading],[important_information] from [Important_information] ORDER BY [id_important_information] desc", connection);
+                ImportantInformationAdapter.SelectCommand = new SqlCommand("select Top(5) [id_important_information],[date_publication],[heading],[important_information] from [Important_information] ORDER BY [id_important_information] desc", connection);
                 ImportantInformationAdapter.Fill(DataTableBaseImportantInformationPresenter);
             }
             News = DataTableBaseNewsPresenter.DefaultView;
