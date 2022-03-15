b155e1cb8dc0fd2c0f66a28564d13b290d385363
Михаил Александрович Александренко <98051902+Bobchinskey@users.noreply.github.com>
2022-03-15 21:00:29 +0400

    Мелкие изменения

---
+++ TEST/TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dataTable= new DataTable();
+            DateTime date = new DateTime (2022,3,9);
+            DateTime datef = new DateTime(2022, 4, 9);
+            
+            while (date<datef)
+            {
+                date = date.AddDays(1);
+                dataTable.Columns.Add(Convert.ToString(date), typeof(string));
+            }
+            dataTable.Rows.Add("se", typeof(string));
+            Console.WriteLine(dataTable);
+        }
+    }
+}
--- TEST/WPFTestStyle/MainWindow.xaml.cs
+++ TEST/WPFTestStyle/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,29 @@ namespace WPFTestStyle
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DataTable dataTable = new DataTable();
+
         public MainWindow()
         {
             InitializeComponent();
+            SortedSet();
+        }
+        
+        public void SortedSet()
+        {
+            
+            DateTime date = new DateTime(2022, 3, 9);
+            DateTime datef = new DateTime(2022, 3, 11);
+            while (date < datef)
+            {
+                date = date.AddDays(1);
+                dataTable.Columns.Add(Convert.ToString(date), typeof(string));
+            }
+            dataTable.Rows.Add(typeof(string));
+            dataTable.Rows.Add(typeof(string));
+            dataTable.Rows.Add("wdadw","wdawdaws");
+            sd.DataContext = dataTable.DefaultView;
+            
         }
     }
 }
